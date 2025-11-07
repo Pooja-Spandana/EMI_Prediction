6bc0850ba97b0417aa75b0eb2d8e12f62395c7c9
Pooja-Spandana <spandana0425@gmail.com>
2025-11-07 21:06:17 +0530

    Project structure updated

---
+++ src/exception.py
@@ -0,0 +1,25 @@
+import sys
+from src.logger import logging
+
+def error_message_detail(error: Exception, error_detail=sys) -> str:
+    _,_,exc_tb = error_detail.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    error_message = (
+        f"Error occurred in python script [{file_name}], "
+        f"line number [{exc_tb.tb_lineno}] "
+        f"error message [{str(error)}]"
+    )
+
+    return error_message
+
+# Custom Exception Class
+class CustomException(Exception):
+    def __init__(self, error_message: str | Exception, error_detail=sys):
+        super().__init__(error_message)
+        self.error_message = error_message_detail(
+            error_message if isinstance(error_message, Exception) else Exception(error_message),
+            error_detail=error_detail
+        )
+    
+    def __str__(self) -> str:
+        return self.error_message
